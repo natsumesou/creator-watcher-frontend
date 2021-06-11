f59fd071074cbc9fc039c2867e9f650639496c19
natsumesou <natsumesouxx@gmail.com>
2021-06-12 04:15:28 +0900

    ウィークリーランキングのOGPを適切なものに置き換えた

--- src/components/templates/RankingWeeklyPage.tsx
+++ src/components/templates/RankingWeeklyPage.tsx
@@ -8,6 +8,9 @@ const RankingWeeklyPage = ({pageContext}) => {
   const notices = [
     "ウィークリーランキングは毎週月曜の朝5時過ぎに前週分のスパチャ金額・メンバー加入数を集計しています",
   ]
+  const weeklyOGPLink = "https://drive.google.com/uc?id=1ormi4L--eUs90Zop0cjAe4gW0KnDJw9M";
+  site.siteMetadata.defaultImage = weeklyOGPLink;
+  console.log(site.siteMetadata);
 
   return (
     <Box>
