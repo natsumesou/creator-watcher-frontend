e1b08f1efda4aef73b262904d8ba234780f1647f
natsumesou <natsumesouxx@gmail.com>
2021-06-11 17:12:12 +0900

    fix text

--- src/components/templates/RankingDailyPage.tsx
+++ src/components/templates/RankingDailyPage.tsx
@@ -6,7 +6,7 @@ import { Ranking } from '../organisms/Ranking';
 const RankingPage = ({pageContext}) => {
   const { site } = pageContext;
   const notices = [
-    "デイリーランキングは毎日朝5時過ぎに前日5時〜当日5時前後の枠のスパチャ金額・メンバー加入数を集計しています",
+    "デイリーランキングは毎日朝5時過ぎに前日の朝5時〜当日の朝5時前後の枠のスパチャ金額・メンバー加入数を集計しています",
   ]
 
   return (
