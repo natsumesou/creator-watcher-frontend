6551c2958bdfe689ca56b3e803055dc62142533e
natsumesou <natsumesouxx@gmail.com>
2021-06-13 03:09:11 +0900

    スマホでタイトルが二段になるのを修正

--- src/components/atoms/TitleButton.tsx
+++ src/components/atoms/TitleButton.tsx
@@ -1,14 +1,26 @@
-import { Button, Typography } from '@material-ui/core'
+import { Button, createStyles, makeStyles, Theme, Typography } from '@material-ui/core'
 import React from 'react'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    text: {
+      [theme.breakpoints.down('xs')]: {
+        fontSize: theme.typography.h3.fontSize,
+      },
+    },
+  }),
+);
 
 type Props = {
   title: string,
 }
 
 export const TitleButton: React.FC<Props> = ({title}) => {
+  const classes = useStyles();
+
   return (
     <Button href="/">
-      <Typography variant="h1">
+      <Typography variant="h1" className={classes.text}>
         {title}
       </Typography>
     </Button>
