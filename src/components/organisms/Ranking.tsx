f9a58bde2b595630343170cfdbb9ffdb7d61adc6
natsumesou <natsumesouxx@gmail.com>
2021-06-14 21:07:45 +0900

    flexboxだと広告領域が計測出来ないらしいのでblock要素に変更した

--- src/components/organisms/Ranking.tsx
+++ src/components/organisms/Ranking.tsx
@@ -26,6 +26,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     ads: {
       minHeight: '250px',
+      display: 'block',
     }
   }),
 );
--- src/components/organisms/Timeline.tsx
+++ src/components/organisms/Timeline.tsx
@@ -19,6 +19,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     ads: {
       minHeight: '250px',
+      display: 'block',
     }
   }),
 );
