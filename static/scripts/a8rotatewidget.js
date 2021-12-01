c8bd027d10ad22dc3171d556fae0361193152802
Yoshiaki Natsume <yoshiaki@natsu.me>
2021-12-01 20:28:24 +0900

    update ads

--- static/scripts/a8rotatewidget.js
+++ static/scripts/a8rotatewidget.js
@@ -11,7 +11,10 @@
         let tag = "";
         if (rand <= 0.20) {
             // marrish
-            tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HIU7T+7YYYCY+3N2M+739TD" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=210830969482&wid=001&eno=01&mid=s00000016987001191000&mc=1"></a><img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3HIU7T+7YYYCY+3N2M+739TD" alt="">';
+            // tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HIU7T+7YYYCY+3N2M+739TD" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=210830969482&wid=001&eno=01&mid=s00000016987001191000&mc=1"></a><img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3HIU7T+7YYYCY+3N2M+739TD" alt="">';
+            // ad(areas[i], tag);
+            // カードラボ senren
+            tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fcorner%2Fcc%2Fgemakuji_senren%2Fcd%2F669%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-senren.jpeg"></a>';
             ad(areas[i], tag);
             continue;
         } else if (rand <= 0.4) {
@@ -20,7 +23,7 @@
             ad(areas[i], tag);
             continue;
         } else if (rand <= 0.6) {
-            // カードラボ
+            // カードラボ tenkaiji
             tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fcorner%2Fcc%2Ftenkaitukasa_glasses%2Fcd%2F664%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-tenkaiji.jpeg"></a>'
             ad(areas[i], tag);
             continue;
@@ -31,7 +34,10 @@
             continue;
         } else {
             // torico
-            tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HGMEE+FXXVXU+4MD6+61C2P" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www29.a8.net/svt/bgt?aid=210727526964&wid=001&eno=01&mid=s00000021561001014000&mc=1"></a><img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3HGMEE+FXXVXU+4MD6+61C2P" alt="">';
+            // tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HGMEE+FXXVXU+4MD6+61C2P" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www29.a8.net/svt/bgt?aid=210727526964&wid=001&eno=01&mid=s00000021561001014000&mc=1"></a><img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3HGMEE+FXXVXU+4MD6+61C2P" alt="">';
+            // ad(areas[i], tag);
+            // カードラボ スパイ教室
+            tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fcorner%2Fcc%2Fgemakuji_spy%2Fcd%2F665%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-spy.jpeg"></a>'
             ad(areas[i], tag);
             continue;
         }
