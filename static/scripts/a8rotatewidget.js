6d0ad1bdc92dd0aaa9bd11e7735b04f40a9552c1
Yoshiaki Natsume <yoshiaki@natsu.me>
2022-01-07 18:30:41 +0900

    update ads

--- static/scripts/a8rotatewidget.js
+++ static/scripts/a8rotatewidget.js
@@ -13,8 +13,8 @@
             // marrish
             // tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HIU7T+7YYYCY+3N2M+739TD" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=210830969482&wid=001&eno=01&mid=s00000016987001191000&mc=1"></a><img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3HIU7T+7YYYCY+3N2M+739TD" alt="">';
             // ad(areas[i], tag);
-            // カードラボ senren
-            tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fcorner%2Fcc%2Fgemakuji_senren%2Fcd%2F669%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-senren.jpeg"></a>';
+            // カードラボ lovelive
+            tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fpn%2Fpd%2F10584253%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-loveliveclearfile.jpg"></a>';
             ad(areas[i], tag);
             continue;
         } else if (rand <= 0.4) {
@@ -38,8 +38,8 @@
             // torico
             // tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HGMEE+FXXVXU+4MD6+61C2P" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www29.a8.net/svt/bgt?aid=210727526964&wid=001&eno=01&mid=s00000021561001014000&mc=1"></a><img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3HGMEE+FXXVXU+4MD6+61C2P" alt="">';
             // ad(areas[i], tag);
-            // カードラボ スパイ教室
-            tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fcorner%2Fcc%2Fgemakuji_spy%2Fcd%2F665%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-spy.jpeg"></a>'
+            // カードラボ uma
+            tag = '<a href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fpn%2Fpd%2F10580207%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-umamusume.jpg"></a>'
             ad(areas[i], tag);
             continue;
         }
