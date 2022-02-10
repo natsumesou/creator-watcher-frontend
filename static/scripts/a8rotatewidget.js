2c989f9b694b9fcd88b3b25e9e5e98f2d087a1e0
Yoshiaki Natsume <yoshiaki@natsu.me>
2022-02-10 18:43:56 +0900

    update ads

--- static/scripts/a8rotatewidget.js
+++ static/scripts/a8rotatewidget.js
@@ -9,26 +9,26 @@
     for(var i = 0; i < areas.length; i++) {
         const rand = Math.random();
         let tag = "";
-        if (rand <= 0.20) {
+        if (rand <= 0.1) {
             // marrish
             // tag = '<a target="_blank" rel="noopener noreferrer" href="https://px.a8.net/svt/ejp?a8mat=3HIU7T+7YYYCY+3N2M+739TD" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=210830969482&wid=001&eno=01&mid=s00000016987001191000&mc=1"></a><img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3HIU7T+7YYYCY+3N2M+739TD" alt="">';
             // カードラボ lovelive
             tag = '<a target="_blank" rel="noopener noreferrer" href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fpn%2Fpd%2F10578791%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-lovelive-sunshine.jpg"></a>';
             ad(areas[i], tag);
             continue;
-        } else if (rand <= 0.4) {
+        } else if (rand <= 0.5) {
             // まんが王国
             // tag = '<a target="_blank" rel="noopener noreferrer" href="https://px.a8.net/svt/ejp?a8mat=3HGMEE+FXCGC2+4QGO+5ZMCH" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www20.a8.net/svt/bgt?aid=210727526963&wid=001&eno=01&mid=s00000022092001006000&mc=1"></a><img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=3HGMEE+FXCGC2+4QGO+5ZMCH" alt="">';
-            // カードラボ kinoko
-            tag = '<a target="_blank" rel="noopener noreferrer" href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fpn%2Fpd%2F10591800%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/konomi.jpg"></a>'
+            // カードラボ pekora
+            tag = '<a target="_blank" rel="noopener noreferrer" href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fpn%2Fpd%2F10593149%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-pekora.jpg"></a>'
             ad(areas[i], tag);
             continue;
-        } else if (rand <= 0.6) {
+        } else if (rand <= 0.75) {
             // カードラボ tenkaiji
             tag = '<a target="_blank" rel="noopener noreferrer" href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fcorner%2Fcc%2Ftenkaitukasa_glasses%2Fcd%2F664%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-tenkaiji.jpeg"></a>'
             ad(areas[i], tag);
             continue;
-        } else if (rand <= 0.8) {
+        } else if (rand <= 0.9) {
             // U-NEXT
             // tag = '<a target="_blank" rel="noopener noreferrer" href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FOEY9E+3250+6PC9T" rel="nofollow"><img border="0" width="100%" max-height="90" alt="" src="https://www26.a8.net/svt/bgt?aid=210730565948&wid=001&eno=01&mid=s00000014274001126000&mc=1"></a><img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=3HGOQT+FOEY9E+3250+6PC9T" alt="">'
             // カードラボ nijisanji
