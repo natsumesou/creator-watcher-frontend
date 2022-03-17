31bd1fd7b501dd180e35f33d03ecf6816f743e52
Yoshiaki Natsume <yoshiaki@natsu.me>
2022-03-18 04:33:56 +0900

    update ads

--- static/scripts/a8rotatewidget.js
+++ static/scripts/a8rotatewidget.js
@@ -12,8 +12,8 @@
         if (rand <= 0.2) {
             // marrish
             // tag = '<a target="_blank" rel="noopener noreferrer" href="https://px.a8.net/svt/ejp?a8mat=3HIU7T+7YYYCY+3N2M+739TD" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=210830969482&wid=001&eno=01&mid=s00000016987001191000&mc=1"></a><img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3HIU7T+7YYYCY+3N2M+739TD" alt="">';
-            // カードラボ iriya
-            tag = '<a target="_blank" rel="noopener noreferrer" href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fcorner%2Fcc%2Fgemakuji_prisma-illya_movie%2Fcd%2F683%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-iriya.jpg"></a>';
+            // カードラボ uma
+            tag = '<a target="_blank" rel="noopener noreferrer" href="https://px.a8.net/svt/ejp?a8mat=3HGOQT+FTRUPE+4AHY+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.gamers.co.jp%2Fpn%2Fpd%2F10598049%2F" rel="nofollow"><img border="0" width="100%" alt="" src="/images/ads/cardlab-umamusume-toukaiteio.jpg"></a>'
             ad(areas[i], tag);
             continue;
         } else if (rand <= 0.5) {
